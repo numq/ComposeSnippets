5d9972f30cad9229df7c1b0fa313f7a7349f045e
numq <numq.dev@gmail.com>
2022-10-22 17:42:12 +0300

    Provide items moving while dragging

--- app/src/main/java/com/numq/composesnippets/components/reorder/ReorderableColumn.kt
+++ app/src/main/java/com/numq/composesnippets/components/reorder/ReorderableColumn.kt
@@ -7,6 +7,9 @@ import androidx.compose.foundation.lazy.LazyListItemInfo
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Reorder
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,24 +19,27 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 
+
 @Composable
 fun <T> ReorderableColumn(
-    items: List<T>,
+    items: MutableList<T>,
     onMove: (Int, Int) -> Unit,
     onItemContent: @Composable (T) -> Unit
 ) {
 
     val lazyListState = rememberLazyListState()
 
-    var offsetY by remember { mutableStateOf(0f) }
-
     var draggingItem by remember { mutableStateOf<LazyListItemInfo?>(null) }
 
+    var offsetY by remember { mutableStateOf(0f) }
+
     val hoveredItem by derivedStateOf {
-        draggingItem?.let { draggingItem ->
-            val draggingAnchor = draggingItem.offset + offsetY + (draggingItem.size / 2f)
+        draggingItem?.let { dragging ->
             lazyListState.layoutInfo.visibleItemsInfo
-                .firstOrNull { it.offset <= draggingAnchor && draggingAnchor <= it.offset + it.size }
+                .filter { it.key != dragging.key }
+                .firstOrNull { item ->
+                    (dragging.offset + offsetY).toInt() in (item.offset..item.offset + item.size)
+                }
         }
     }
 
@@ -47,52 +53,73 @@ fun <T> ReorderableColumn(
             state = lazyListState
         ) {
             itemsIndexed(items) { index, item ->
-                Column(
+                Row(
                     Modifier
                         .fillMaxWidth()
                         .height(128.dp)
-                        .padding(4.dp)
                         .zIndex(if (draggingItem?.index == index) 1f else 0f)
                         .graphicsLayer {
-                            if (draggingItem?.index == index) {
-                                translationY = offsetY
-                            }
-                        }
-                        .pointerInput(Unit) {
-                            detectDragGestures(onDragStart = {
-                                draggingItem =
-                                    lazyListState.layoutInfo.visibleItemsInfo.getOrNull(index)
-                            }, onDragEnd = {
-                                draggingItem?.let { draggingItem ->
-                                    hoveredItem?.let { hoveredItem ->
-                                        onMove(
-                                            draggingItem.index,
-                                            hoveredItem.index
-                                        )
-                                    }
+                            draggingItem?.let { dragging ->
+                                if (dragging.index == index) {
+                                    translationY = offsetY
                                 }
-                                draggingItem = null
-                                offsetY = 0f
-                            }, onDragCancel = {
-                                offsetY = 0f
-                                draggingItem = null
-                            }) { change, (_, y) ->
-                                change.consumeAllChanges()
-                                offsetY += y
                             }
                         },
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Card(Modifier.weight(1f)) {
-                        Box(
-                            Modifier
-                                .fillMaxWidth()
-                                .weight(1f),
-                            contentAlignment = Alignment.Center
-                        ) {
+                    Card(
+                        Modifier
+                            .graphicsLayer {
+                                if (index == draggingItem?.index) {
+                                    alpha = .5f
+                                    scaleX = .9f
+                                }
+                            }
+                            .weight(1f)
+                            .padding(4.dp)
+                    ) {
+                        Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                             onItemContent(item)
                         }
+                    }
+                    Box(
+                        Modifier
+                            .fillMaxHeight()
+                            .padding(8.dp)
+                            .pointerInput(Unit) {
+                                detectDragGestures(onDragStart = {
+                                    draggingItem =
+                                        lazyListState.layoutInfo.visibleItemsInfo.getOrNull(
+                                            index
+                                        )
+                                }, onDragEnd = {
+                                    draggingItem = null
+                                    offsetY = 0f
+                                }, onDragCancel = {
+                                    offsetY = 0f
+                                    draggingItem = null
+                                }) { change, (_, y) ->
+                                    change.consumeAllChanges()
+                                    offsetY += y
+                                    draggingItem?.let { dragging ->
+                                        hoveredItem?.let { hovered ->
+                                            onMove(
+                                                dragging.index,
+                                                hovered.index
+                                            )
+                                            draggingItem = hovered
+                                            offsetY += ((dragging.index - hovered.index) * dragging.size)
+                                        }
+                                    }
+                                }
+                            },
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            Icons.Rounded.Reorder,
+                            "reorder list"
+                        )
                     }
                 }
             }
