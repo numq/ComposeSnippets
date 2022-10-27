01f762a8fd60ee14efabdcd31cc334b262297f56
numq <numq.dev@gmail.com>
2022-10-27 22:36:28 +0300

    Add brush size changing logic

--- app/src/main/java/com/numq/composesnippets/components/drawing/DrawingScreen.kt
+++ app/src/main/java/com/numq/composesnippets/components/drawing/DrawingScreen.kt
@@ -6,9 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.material.icons.rounded.Redo
@@ -23,17 +21,29 @@ import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.input.pointer.consumeAllChanges
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
+import kotlin.math.sign
 
 @Composable
 fun DrawingScreen() {
 
     val darkTheme = isSystemInDarkTheme()
 
+    val coroutineScope = rememberCoroutineScope()
+
     val currentPoints = remember { mutableStateListOf<Offset>() }
 
     val currentBackStack = remember { mutableStateListOf<DrawingTile>() }
 
     val undoBackStack = remember { mutableStateListOf<DrawingTile>() }
+
+    val (minBrushSize, maxBrushSize) = Pair(4f, 100f)
+
+    var currentBrushSize by remember { mutableStateOf(minBrushSize) }
+
+    var isBrushSizeChanging by remember { mutableStateOf(false) }
 
     var currentColor by remember { mutableStateOf(if (darkTheme) Color.White else Color.Black) }
 
@@ -62,6 +72,7 @@ fun DrawingScreen() {
                     currentBackStack.add(
                         DrawingTile(
                             currentColor,
+                            currentBrushSize,
                             currentPoints.toList()
                         )
                     )
@@ -72,72 +83,115 @@ fun DrawingScreen() {
                 }
             }
         ) {
-            currentBackStack.forEach { (color, points) ->
+            currentBackStack.forEach { (color, brushSize, points) ->
                 drawPoints(
                     points,
                     pointMode = PointMode.Polygon,
-                    color = color
+                    color = color,
+                    strokeWidth = brushSize
                 )
             }
             drawPoints(
                 currentPoints,
                 pointMode = PointMode.Polygon,
-                color = currentColor
+                color = currentColor,
+                strokeWidth = currentBrushSize
             )
         }
-        Box(Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                if (colorPickerVisible) {
-                    Row(
-                        Modifier.padding(4.dp),
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        colors.filter { it != currentColor }.forEach { color ->
-                            ColorItem(color) {
-                                currentColor = color
-                                setColorPickerVisible(false)
+        Box(contentAlignment = Alignment.Center) {
+            Box(Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    if (colorPickerVisible) {
+                        Column(
+                            Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                if (isBrushSizeChanging) {
+                                    Divider(
+                                        Modifier.fillMaxWidth(),
+                                        thickness = currentBrushSize.dp,
+                                        color = currentColor
+                                    )
+                                }
+                                Card {
+                                    Text(
+                                        "${currentBrushSize.roundToInt()}.px",
+                                        Modifier.pointerInput(Unit) {
+                                            detectDragGestures(onDragStart = {
+                                                isBrushSizeChanging = true
+                                            }, onDragCancel = {
+                                                isBrushSizeChanging = false
+                                            }, onDragEnd = {
+                                                isBrushSizeChanging = false
+                                            }) { change, (x, y) ->
+                                                change.consumeAllChanges()
+                                                coroutineScope.launch {
+                                                    currentBrushSize =
+                                                        currentBrushSize.plus(sign(x) * 1)
+                                                            .coerceIn(minBrushSize, maxBrushSize)
+                                                    delay(1000L)
+                                                }
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                            Row(
+                                Modifier.padding(4.dp),
+                                horizontalArrangement = Arrangement.Center,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                colors.filter { it != currentColor }.forEach { color ->
+                                    ColorItem(color) {
+                                        currentColor = color
+                                        setColorPickerVisible(false)
+                                    }
+                                }
                             }
                         }
                     }
-                }
-                Card {
-                    Row(
-                        Modifier.padding(4.dp),
-                        horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        IconButton(onClick = {
-                            currentBackStack.clear()
-                        }) {
-                            Icon(Icons.Rounded.Clear, "remove content")
-                        }
-                        IconButton(onClick = {
-                            if (currentBackStack.isNotEmpty()) {
-                                undoBackStack.add(currentBackStack.last())
-                                currentBackStack.removeLast()
+                    Card {
+                        Row(
+                            Modifier.padding(4.dp),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            IconButton(onClick = {
+                                currentBackStack.clear()
+                            }) {
+                                Icon(Icons.Rounded.Clear, "remove content")
                             }
-                        }) {
-                            Icon(Icons.Rounded.Undo, "undo")
-                        }
-                        IconButton(onClick = {
-                            if (undoBackStack.isNotEmpty()) {
-                                currentBackStack.add(undoBackStack.last())
-                                undoBackStack.removeLast()
+                            IconButton(onClick = {
+                                if (currentBackStack.isNotEmpty()) {
+                                    undoBackStack.add(currentBackStack.last())
+                                    currentBackStack.removeLast()
+                                }
+                            }) {
+                                Icon(Icons.Rounded.Undo, "undo")
                             }
-                        }) {
-                            Icon(Icons.Rounded.Redo, "redo")
-                        }
-                        ColorItem(currentColor) {
-                            setColorPickerVisible(!colorPickerVisible)
+                            IconButton(onClick = {
+                                if (undoBackStack.isNotEmpty()) {
+                                    currentBackStack.add(undoBackStack.last())
+                                    undoBackStack.removeLast()
+                                }
+                            }) {
+                                Icon(Icons.Rounded.Redo, "redo")
+                            }
+                            ColorItem(currentColor) {
+                                setColorPickerVisible(!colorPickerVisible)
+                            }
                         }
                     }
                 }
             }
-
         }
     }
 }
